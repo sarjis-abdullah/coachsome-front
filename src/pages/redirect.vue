2ca5035e3b00e8d83e4af1bf6c95f7a49b644924
hafij <hafijur@tikweb.dk>
2021-06-15 18:56:30 +0600

    Updated

--- src/pages/redirect.vue
+++ src/pages/redirect.vue
@@ -12,11 +12,11 @@ export default {
   }),
   mounted() {
     if (this.$route.query.access_token) {
+      this.$auth.setUserToken(this.$route.query.access_token);
       userApi(this.$axios)
         .authUserInformation()
         .then(({ data }) => {
           this.$auth.setUser(data.user);
-          this.$auth.setUserToken(this.$route.query.access_token);
           if (this.$auth.hasRole(["coach"])) {
             this.$router.push(this.localePath(pathData.coach.editProfile));
           } else if (this.$auth.hasRole(["athlete"])) {
