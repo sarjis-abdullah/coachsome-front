5bfb8179dbda73ffe5699f269d5de3805a44377b
hafij <hafijur@tikweb.dk>
2021-06-21 18:36:06 +0600

    Hide countdown

--- src/pages/packages/_id.vue
+++ src/pages/packages/_id.vue
@@ -349,9 +349,9 @@
                             </i18n>
                           </div>
                         </div>
-                        <div class="quick-booking__timer">
+                        <!-- <div class="quick-booking__timer">
                           <div class="timer" ref="timerDisplay"></div>
-                        </div>
+                        </div> -->
                         <div class="quick-booking__chat-btn mt-10">
                           <v-btn
                             :loading="isChatBtnLoading"
@@ -533,9 +533,9 @@ export default {
           storageHelper.set("booking", booking);
         }
       }
-      if (val == 3) {
-        this.startTimer(60 * 7);
-      }
+      // if (val == 3) {
+      //   this.startTimer(60 * 7);
+      // }
     },
     "packageInfo.chargeBox.personNumbers.value": {
       handler(val) {
@@ -728,36 +728,36 @@ export default {
         })
       );
     },
-    startTimer(duration) {
-      let display = this.$refs.timerDisplay;
-      var start = Date.now(),
-        diff,
-        minutes,
-        seconds;
-      function timer() {
-        // get the number of seconds that have elapsed since
-        // startTimer() was called
-        diff = duration - (((Date.now() - start) / 1000) | 0);
+    // startTimer(duration) {
+    //   let display = this.$refs.timerDisplay;
+    //   var start = Date.now(),
+    //     diff,
+    //     minutes,
+    //     seconds;
+    //   function timer() {
+    //     // get the number of seconds that have elapsed since
+    //     // startTimer() was called
+    //     diff = duration - (((Date.now() - start) / 1000) | 0);
 
-        // does the same job as parseInt truncates the float
-        minutes = (diff / 60) | 0;
-        seconds = diff % 60 | 0;
+    //     // does the same job as parseInt truncates the float
+    //     minutes = (diff / 60) | 0;
+    //     seconds = diff % 60 | 0;
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+    //     minutes = minutes < 10 ? "0" + minutes : minutes;
+    //     seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+    //     display.textContent = minutes + ":" + seconds;
 
-        if (diff <= 0) {
-          // add one second so that the count down starts at the full duration
-          // example 05:00 not 04:59
-          start = Date.now() + 1000;
-        }
-      }
-      // we don't want to wait a full second before the timer starts
-      timer();
-      setInterval(timer, 1000);
-    },
+    //     if (diff <= 0) {
+    //       // add one second so that the count down starts at the full duration
+    //       // example 05:00 not 04:59
+    //       start = Date.now() + 1000;
+    //     }
+    //   }
+    //   // we don't want to wait a full second before the timer starts
+    //   timer();
+    //   setInterval(timer, 1000);
+    // },
 
     setStepToStorage(step) {
       let booking = storageHelper.get("booking");
