5dc3688ff0a544e56099af4a7daa79445f779ca4
sarjis-abdullah0 <sarjis@tikweb.dk>
2022-05-09 18:35:25 +0600

    three dot dropdown added

--- src/components/contacts/CoachContactsSingleData.vue
+++ src/components/contacts/CoachContactsSingleData.vue
@@ -24,10 +24,10 @@
           alt="contacts-package"
           class="pr-2"
         />
-        {{contact.package.name}}
+        {{ contact.package.name }}
       </span>
-  
-      <span v-else class='no-package'>No Active Packages	</span>
+
+      <span v-else class="no-package">No Active Packages </span>
     </td>
     <td class="text-center">1 month ago</td>
     <td class="cursor-pointer">
@@ -38,10 +38,37 @@
           @click="gotoChat"
         />
         <img :src="require('@/assets/img/svg-icons/chat.svg')" alt="chat" />
-        <img
-          :src="require('@/assets/img/svg-icons/three-dot-horizontal.svg')"
-          alt="three-dot-horizontal"
-        />
+        <!-- Three Dots horizontal -->
+        <template>
+          <div class="text-center">
+            <v-menu
+              top
+              offset-y
+              close-on-content-click
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <img
+                  v-bind="attrs"
+                  v-on="on"
+                  :src="
+                    require('@/assets/img/svg-icons/three-dot-horizontal.svg')
+                  "
+                  alt="three-dot-horizontal"
+                />
+              </template>
+
+              <v-list>
+                <v-list-item v-for="(item, index) in moreItems" :key="index">
+                  <v-list-item-title class="cursor-pointer">
+                    <span :style="`color: ${item.color}`">
+                      {{ item.text }}
+                    </span>
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </div>
+        </template>
       </section>
     </td>
   </tr>
@@ -53,24 +80,37 @@ export default {
     contact: {
       type: Object,
       default: () => ({})
-    },
+    }
+  },
+  data() {
+    return {
+      moreItems: [
+        {
+          text: "Resend Invitation Email",
+          color: "#000000"
+        },
+        {
+          text: "Remove Contact",
+          color: "#FF633F"
+        }
+      ]
+    };
   },
   methods: {
     gotoChat() {
       console.log("gotoChat");
     }
-  },
+  }
 };
 </script>
 <style scoped>
 .contacts-single-data {
-  border: 1px solid #ECF2F7;
+  border: 1px solid #ecf2f7;
 }
 .no-package {
-  color: #9FAEC2;
+  color: #9faec2;
 }
 .cursor-pointer {
   cursor: pointer;
 }
 </style>
-
