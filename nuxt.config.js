3dabdb227ad446e1d77ed3f5dc3917da75ac7c29
shawon3719 <shawon3719@gmail.com>
2022-05-26 11:26:07 +0600

    fix file delete issue on update

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,8 +19,8 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { name: "apple-mobile-web-app-status-bar-style", content: "red" },
-      { name: "theme-color",  content: "#f50505" },
+      { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent"},
+      { name: "theme-color",  content: "#000000" },
       {
         name: "facebook-domain-verification",
         content: "7ilgh3d5ukfpfwwspw4chai9zokx7u"
