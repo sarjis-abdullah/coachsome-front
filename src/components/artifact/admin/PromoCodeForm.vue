df4086fedd5d3aab282ff9336bf74efe11b3ae38
sarjis-abdullah0 <sarjis@tikweb.dk>
2022-05-31 13:43:50 +0600

    component separated for re-use purpose

---
+++ src/components/artifact/admin/PromoCodeForm.vue
@@ -0,0 +1,306 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-container>
+        <v-row class="pt-5">
+          <v-col class="text-left font-color-1 text-h6 text-capitalize">
+            <FormHeader @close="closePromoCodeDialog" :title="dialogTitle" />
+          </v-col>
+        </v-row>
+        <v-form ref="form" v-model="valid">
+          <!-- Promo name -->
+          <v-row>
+            <v-col cols="12">
+              <div class="subtitle-2 pb-2">
+                Promo name (For tracking purpose)
+              </div>
+              <v-text-field
+                :rules="[v => !!v || 'Name is required']"
+                dense
+                v-model="defaultForm.name"
+                label="Enter a promo name"
+                outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <!-- Promo code -->
+          <v-row>
+            <v-col cols="12">
+              <div class="subtitle-2 pb-2">
+                Promo code (the actual code)
+              </div>
+              <v-text-field
+                :rules="[v => !!v || 'Promo code is required']"
+                dense
+                v-model="defaultForm.code"
+                label="Enter a promo code name"
+                outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <!-- Promotion type -->
+          <v-row>
+            <v-col cols="12">
+              <div class="subtitle-2">Promotion type</div>
+              <v-radio-group v-model="defaultForm.type" hide-details>
+                <v-radio
+                  v-for="item in types"
+                  :key="item.id"
+                  :label="item.name"
+                  :value="item.id"
+                  color="primary-light-1"
+                  :rules="[v => !!v || 'Promotion type is required']"
+                ></v-radio>
+              </v-radio-group>
+            </v-col>
+          </v-row>
+
+          <!-- currency & discount ammount -->
+          <v-row v-if="isFixed">
+            <v-col>
+              <div class="subtitle-2 pb-2">Currency</div>
+              <v-select
+                v-model="defaultForm.currency"
+                :items="currencies"
+                item-text="displayText"
+                item-value="id"
+                outlined
+                dense
+                label="Select currency"
+                :rules="[v => !!v || 'Currency is required']"
+              ></v-select>
+            </v-col>
+            <v-col>
+              <div class="subtitle-2 pb-2">
+                Discount amount
+              </div>
+              <v-text-field
+                type="number"
+                dense
+                v-model="defaultForm.discount"
+                label="Select amount"
+                outlined
+                min="0"
+                :rules="[v => !!v || 'Discount amount is required']"
+              >
+                <template v-slot:prepend-inner>
+                  <div>
+                    {{ defaultForm.symbol }}
+                  </div>
+                </template>
+              </v-text-field>
+            </v-col>
+          </v-row>
+
+          <!-- Percentage off -->
+          <v-row v-if="!isFixed">
+            <v-col cols="12">
+              <div class="subtitle-2 pb-2">Percentage off</div>
+              <v-text-field
+                type="number"
+                :rules="[v => !!v || 'Percentage off is required']"
+                min="0"
+                max="100"
+                dense
+                v-model="defaultForm.percentageOff"
+                label="Select discount percentage"
+                outlined
+              >
+                <template v-slot:append>
+                  <div>
+                    %
+                  </div>
+                </template>
+              </v-text-field>
+            </v-col>
+          </v-row>
+
+          <!-- Duration -->
+          <v-row>
+            <v-col cols="12">
+              <div class="subtitle-2 pb-2">Duration</div>
+              <v-select
+                v-model="defaultForm.duration"
+                :rules="[v => !!v || 'Duration is required']"
+                dense
+                :items="durations"
+                item-text="name"
+                item-value="id"
+                label="Select duration"
+                outlined
+              >
+              </v-select>
+            </v-col>
+          </v-row>
+        </v-form>
+        <v-row>
+          <v-col>
+            <v-btn
+              v-if="!editMode"
+              :loading="isLoading"
+              depressed
+              color="primary-light-1"
+              dark
+              @click="handleCreateBtnClick"
+            >
+              Create
+            </v-btn>
+            <v-btn
+              v-if="editMode"
+              :loading="isLoading"
+              depressed
+              color="primary-light-1"
+              dark
+              @click="handleUpdateBtnClick"
+            >
+              Update
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { adminPromoCodeApi } from "@/api";
+import { currencyService } from "@/services";
+import { pathData } from "@/data";
+import FormHeader from "@/components/layout/global/FormHeader";
+import MobileTopNav from "@/components/layout/global/MobileTopNav";
+const formItem = {
+  id: null,
+  name: "",
+  code: "",
+  type: 1,
+  currency: null,
+  symbol: null,
+  discount: null,
+  percentageOff: null,
+  duration: null
+};
+export default {
+  components: {
+    FormHeader,
+    MobileTopNav
+  },
+  props: {
+    editMode: {
+      type: Boolean,
+      default: false
+    },
+    types: {
+      type: Array,
+      default: () => []
+    },
+    durations: {
+      type: Array,
+      default: () => []
+    },
+    currencies: {
+      type: Array,
+      default: () => []
+    },
+    editableFormData: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      search: "",
+      dialog: false,
+      isLoading: false,
+      valid: true,
+      promoCodes: [],
+      defaultForm: JSON.parse(JSON.stringify(formItem)),
+      resetForm: JSON.parse(JSON.stringify(formItem))
+    };
+  },
+  computed: {
+    isFixed() {
+      return this.defaultForm.type == 2;
+    },
+    dialogTitle() {
+      return this.editMode ? "Edit Promo Code" : "Create Promo Code";
+    }
+  },
+  watch: {
+    "defaultForm.currency": function(val) {
+      if (val) {
+        this.defaultForm.symbol = this.currencies.find(
+          item => item.id == val
+        ).symbol;
+      }
+    },
+    editableFormData: {
+      immediate: true,
+      deep: true,
+      handler(newValue, oldValue) {
+        if (
+          this.editableFormData &&
+          Object.keys(this.editableFormData).length
+        ) {
+          this.defaultForm = JSON.parse(JSON.stringify(this.editableFormData));
+        }
+      }
+    }
+  },
+  methods: {
+    handleUpdateBtnClick() {
+      if (this.$refs.form.validate()) {
+        this.isLoading = true;
+        adminPromoCodeApi(this.$axios)
+          .update(this.defaultForm.id, { ...this.defaultForm })
+          .then(({ data }) => {
+            if (data.promoCode) {
+              this.$emit("updatePromoInUpdateMode", data);
+              this.$toast.success("Successfully updated");
+            }
+          })
+          .catch(({ response }) => {
+            if (response.data.message) {
+              this.$toast.error(response.data.message);
+            }
+          })
+          .finally(() => {
+            this.isLoading = false;
+            this.closePromoCodeDialog();
+          });
+      }
+    },
+    handleCreateBtnClick() {
+      if (this.$refs.form.validate()) {
+        this.isLoading = true;
+        adminPromoCodeApi(this.$axios)
+          .store({ ...this.defaultForm })
+          .then(({ data }) => {
+            this.$emit("updatePromoInCreateMode", data);
+
+            this.$toast.success("Successfully created a new item");
+          })
+          .catch(({ response }) => {
+            if (response.data.message) {
+              this.$toast.error(response.data.message);
+            }
+          })
+          .finally(() => {
+            this.isLoading = false;
+            this.closePromoCodeDialog();
+          });
+      }
+    },
+    closePromoCodeDialog() {
+      this.defaultForm = JSON.parse(JSON.stringify(this.resetForm))
+      if (this.$refs.form) {
+        this.$refs.form.resetValidation();
+      }
+      this.$emit("closePromoCodeDialog");
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
--- src/pages/admin/promos.vue
+++ src/pages/admin/promos.vue
@@ -85,171 +85,15 @@
 
           <!-- Create Dialog -->
           <v-dialog v-model="dialog" max-width="600" @click:outside="closePromoCodeDialog">
-            <v-card>
-              <v-card-text>
-                <v-container>
-                  <v-row class="pt-5">
-                    <v-col
-                      class="text-left font-color-1 text-h6 text-capitalize"
-                    >
-                      <FormHeader @close="closePromoCodeDialog" :title="dialogTitle"/>
-                    </v-col>
-                  </v-row>
-                  <v-form ref="form" v-model="valid">
-                    <!-- Promo name -->
-                    <v-row>
-                      <v-col cols="12">
-                        <div class="subtitle-2 pb-2">
-                          Promo name (For tracking purpose)
-                        </div>
-                        <v-text-field
-                          :rules="[v => !!v || 'Name is required']"
-                          dense
-                          v-model="defaultForm.name"
-                          label="Enter a promo name"
-                          outlined
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-
-                    <!-- Promo code -->
-                    <v-row>
-                      <v-col cols="12">
-                        <div class="subtitle-2 pb-2">
-                          Promo code (the actual code)
-                        </div>
-                        <v-text-field
-                          :rules="[v => !!v || 'Promo code is required']"
-                          dense
-                          v-model="defaultForm.code"
-                          label="Enter a promo code name"
-                          outlined
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-
-                    <!-- Promotion type -->
-                    <v-row>
-                      <v-col cols="12">
-                        <div class="subtitle-2">Promotion type</div>
-                          <v-radio-group v-model="defaultForm.type" hide-details>
-                            <v-radio
-                              v-for="item in types"
-                              :key="item.id"
-                              :label="item.name"
-                              :value="item.id"
-                              color="primary-light-1"
-                              :rules="[v => !!v || 'Promotion type is required']"
-                            ></v-radio>
-                          </v-radio-group>
-                      </v-col>
-                    </v-row>
-
-                    <!-- currency & discount ammount -->
-                    <v-row v-if="isFixed">
-                      <v-col>
-                        <div class="subtitle-2 pb-2">Currency</div>
-                        <v-select
-                          v-model="defaultForm.currency"
-                          :items="currencies"
-                          item-text="displayText"
-                          item-value="id"
-                          outlined
-                          dense
-                          label="Select currency"
-                          :rules="[v => !!v || 'Currency is required']"
-                        ></v-select>
-                      </v-col>
-                      <v-col>
-                        <div class="subtitle-2 pb-2">
-                          Discount amount
-                        </div>
-                        <v-text-field
-                          type="number"
-                          dense
-                          v-model="defaultForm.discount"
-                          label="Select amount"
-                          outlined
-                          min="0"
-                          :rules="[v => !!v || 'Discount amount is required']"
-                        >
-                          <template v-slot:prepend-inner>
-                            <div>
-                              {{ defaultForm.symbol }}
-                            </div>
-                          </template>
-                        </v-text-field>
-                      </v-col>
-                    </v-row>
-
-                    <!-- Percentage off -->
-                    <v-row v-if="!isFixed">
-                      <v-col cols="12">
-                        <div class="subtitle-2 pb-2">Percentage off</div>
-                        <v-text-field
-                          type="number"
-                          :rules="[v => !!v || 'Percentage off is required']"
-                          min="0"
-                          max="100"
-                          dense
-                          v-model="defaultForm.percentageOff"
-                          label="Select discount percentage"
-                          outlined
-                        >
-                          <template v-slot:append>
-                            <div>
-                              %
-                            </div>
-                          </template>
-                        </v-text-field>
-                      </v-col>
-                    </v-row>
-
-                    <!-- Duration -->
-                    <v-row>
-                      <v-col cols="12">
-                        <div class="subtitle-2 pb-2">Duration</div>
-                        <v-select
-                          v-model="defaultForm.duration"
-                          :rules="[v => !!v || 'Duration is required']"
-                          dense
-                          :items="durations"
-                          item-text="name"
-                          item-value="id"
-                          label="Select duration"
-                          outlined
-                        >
-                        </v-select>
-                      </v-col>
-                    </v-row>
-                  </v-form>
-                  <v-row>
-                    <v-col>
-                      <v-btn
-                        v-if="!editMode"
-                        :loading="isLoading"
-                        depressed
-                        color="primary-light-1"
-                        dark
-                        @click="handleCreateBtnClick"
-                      >
-                        Create
-                      </v-btn>
-                      <v-btn
-                        v-if="editMode"
-                        :loading="isLoading"
-                        depressed
-                        color="primary-light-1"
-                        dark
-                        @click="handleUpdateBtnClick"
-                      >
-                        Update
-                      </v-btn>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-            </v-card>
+            <PromoCodeForm 
+            :types="types" 
+            :currencies="currencies" 
+            :durations="durations" 
+            :editMode="editMode" 
+            :editableFormData="defaultForm" 
+            @updatePromoInUpdateMode="updatePromoInUpdateMode"
+            @updatePromoInCreateMode="updatePromoInCreateMode"
+            @closePromoCodeDialog="closePromoCodeDialog"/>
           </v-dialog>
           <!-- Create Dialog -->
         </v-card>
@@ -263,11 +107,12 @@ import { adminPromoCodeApi } from "@/api";
 import { currencyService } from "@/services";
 import { pathData } from "@/data";
 import MobileTopNav from '@/components/layout/global/MobileTopNav'
-import FormHeader from '@/components/layout/global/FormHeader'
+
+import PromoCodeForm from '@/components/artifact/admin/PromoCodeForm'
 
 export default {
   layout: "admin",
-  components: {MobileTopNav, FormHeader},
+  components: {MobileTopNav, PromoCodeForm},
   data() {
     return {
       search: "",
@@ -325,23 +170,9 @@ export default {
         }
       }
     },
-    "defaultForm.currency": function(val) {
-      if (val) {
-        this.defaultForm.symbol = this.currencies.find(
-          item => item.id == val
-        ).symbol;
-      }
-    },
     dialog: function(val) {}
   },
-  computed: {
-    isFixed() {
-      return this.defaultForm.type == 2;
-    },
-    dialogTitle() {
-      return this.editMode ? "Edit Promo Code" : "Create Promo Code";
-    }
-  },
+  
   created() {
     adminPromoCodeApi(this.$axios)
       .get()
@@ -392,7 +223,6 @@ export default {
             };
           });
         }
-        console.log(this.currencies);
       });
   },
   methods: {
@@ -405,44 +235,7 @@ export default {
         "_blank"
       );
     },
-    handleUpdateBtnClick() {
-      if (this.$refs.form.validate()) {
-        this.isLoading = true;
-        adminPromoCodeApi(this.$axios)
-          .update(this.defaultForm.id, { ...this.defaultForm })
-          .then(({ data }) => {
-            if (data.promoCode) {
-              let index = this.promoCodes.findIndex(
-                promoCode => promoCode.id == data.promoCode.id
-              );
-              if (index != undefined) {
-                this.promoCodes.splice(index, 1, {
-                  id: data.promoCode.id,
-                  name: data.promoCode.name,
-                  code: data.promoCode.code,
-                  discount: data.promoCode.discount,
-                  currency: data.promoCode.currency,
-                  duration: data.promoCode.duration,
-                  used: data.promoCode.totalUsed,
-                  percentageOff: data.promoCode.percentageOff,
-                  type: data.promoCode.type
-                });
-              }
-              this.dialog = false;
-              this.$toast.success("Successfully updated");
-            }
-          })
-          .catch(({ response }) => {
-            if (response.data.message) {
-              this.$toast.error(response.data.message);
-            }
-          })
-          .finally(() => {
-            this.isLoading = false;
-            this.closePromoCodeDialog()
-          });
-      }
-    },
+    
     handleNewBtnClick() {
       this.editMode = false;
       this.dialog = true;
@@ -451,42 +244,7 @@ export default {
         // this.$refs.form.reset();
       });
     },
-    handleCreateBtnClick() {
-      if (this.$refs.form.validate()) {
-        this.isLoading = true;
-        adminPromoCodeApi(this.$axios)
-          .store({ ...this.defaultForm })
-          .then(({ data }) => {
-            let index = this.promoCodes.findIndex(
-              promoCode => promoCode.id == data.promoCode.id
-            );
-            if (index != undefined) {
-              this.promoCodes.splice(0, 0, {
-                id: data.promoCode.id,
-                name: data.promoCode.name,
-                code: data.promoCode.code,
-                discount: data.promoCode.discount,
-                currency: data.promoCode.currency,
-                duration: data.promoCode.duration,
-                used: data.promoCode.totalUsed,
-                percentageOff: data.promoCode.percentageOff,
-                type: data.promoCode.type
-              });
-            }
-            this.dialog = false;
-            this.$toast.success("Successfully created a new item");
-          })
-          .catch(({ response }) => {
-            if (response.data.message) {
-              this.$toast.error(response.data.message);
-            }
-          })
-          .finally(() => {
-            this.isLoading = false;
-            this.closePromoCodeDialog()
-          });
-      }
-    },
+    
     editItem(item) {
       this.editMode = true;
       this.dialog = true;
@@ -527,6 +285,44 @@ export default {
         promoCodeDialog: true
       }
       this.$router.push({query})
+    },
+    updatePromoInCreateMode(data){
+      let index = this.promoCodes.findIndex(
+        promoCode => promoCode.id == data.promoCode.id
+      );
+      if (index != undefined) {
+        this.promoCodes.splice(0, 0, {
+          id: data.promoCode.id,
+          name: data.promoCode.name,
+          code: data.promoCode.code,
+          discount: data.promoCode.discount,
+          currency: data.promoCode.currency,
+          duration: data.promoCode.duration,
+          used: data.promoCode.totalUsed,
+          percentageOff: data.promoCode.percentageOff,
+          type: data.promoCode.type
+        });
+      }
+      this.dialog = false;
+    },
+    updatePromoInUpdateMode(data){
+      let index = this.promoCodes.findIndex(
+        promoCode => promoCode.id == data.promoCode.id
+      );
+      if (index != undefined) {
+        this.promoCodes.splice(index, 1, {
+          id: data.promoCode.id,
+          name: data.promoCode.name,
+          code: data.promoCode.code,
+          discount: data.promoCode.discount,
+          currency: data.promoCode.currency,
+          duration: data.promoCode.duration,
+          used: data.promoCode.totalUsed,
+          percentageOff: data.promoCode.percentageOff,
+          type: data.promoCode.type
+        });
+      }
+      this.dialog = false;
     },
   }
 };
