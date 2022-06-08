3d7ab3cf246d14a625eb8e2f755b9b1918b94a07
shawon3719 <shawon3719@gmail.com>
2022-06-08 13:57:03 +0600

    fixed redirect issues

--- src/middleware/home.js
+++ src/middleware/home.js
@@ -1,14 +1,16 @@
 import { pathData, roleData } from "@/data";
 export default function({ $auth, redirect }) {
-  if($auth && $auth.user && $auth.user.roles[0].name == "superadmin" || $auth.user.roles[0].name == "admin" || $auth.user.roles[0].name == "staff"){
+ if($auth && $auth.loggedIn){
+  if($auth.user && $auth.user.roles && $auth.user.roles[0].name == "superadmin" || $auth.user.roles[0].name == "admin" || $auth.user.roles[0].name == "staff"){
     console.log("admin");
     redirect(pathData.admin.dashboard);
   }
-  else if($auth && $auth.user && $auth.user.roles[0].name == "coach"){
+  else if($auth.user && $auth.user.roles && $auth.user.roles[0].name == "coach"){
     console.log("coach");
     redirect(pathData.coach.home);
-  }else if($auth && $auth.user && $auth.user.roles[0].name == "athlete"){
+  }else if($auth.user && $auth.user.roles && $auth.user.roles[0].name == "athlete"){
     console.log("athlete");
     redirect(pathData.athlete.home);
   }
+ }
 }
